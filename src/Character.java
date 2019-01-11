d573dc8327606048053deb7f588f545e5d890704
ajdy5510 <ajdy5510@ISS2TWQKH2.scdsb.pri>
2019-01-11 14:33:56 -0500

    STart

---
+++ src/Character.java
@@ -0,0 +1,16 @@
+
+public class Character {
+   static String name;
+   int str,dex,con,intel,wis,cha,ac,perc,hp,spd,cp,sp,ep,gp,pp,pb=2,splslot,splsave,splattack;
+    String feat[];
+    String prof[];
+    String equip[];
+    String armor;
+    String attacks[];
+    static String backstory;
+   public Character(String n){
+       n=name;
+       
+   }
+    
+}
